252cb3ac94e586a1c1e0552b6cdacd9c3c145235
ThuyTien2111 <vanbac21112k2@gmail.com>
2023-11-11 20:27:07 +0700

    done fullstack

--- src/main/java/vn/edu/iuh/fit/dao/PostCommentDao.java
+++ src/main/java/vn/edu/iuh/fit/dao/PostCommentDao.java
@@ -1,5 +1,35 @@
 package vn.edu.iuh.fit.dao;
 
-public class PostComment {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import vn.edu.iuh.fit.entity.PostComment;
+import vn.edu.iuh.fit.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PostCommentDao {
+    private final EntityManager manager;
+    @Autowired
+    public PostCommentDao(EntityManager entityManager) {
+        this.manager = entityManager;
+    }
+    public List<PostComment> getPostCommentByPostID(long postID){
+        List<PostComment> list=new ArrayList<>();
+        try {
+            String sql="SELECT * FROM postcomment\n" +
+                    "WHERE PostID=?";
+            Query query = manager.createNativeQuery(sql, PostComment.class);
+            query.setParameter(1, postID);
+            list=query.getResultList();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 
 }
